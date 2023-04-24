097776dea15b92822de2dc07f705bb31c1139dd3
OrekiYuta <orekiyuta@gmail.com>
2023-04-24 22:23:10 +0800

    build(init): Init Michishirube Project

---
+++ src/main/java/com/elias/mcsrb/McsrbApplication.java
@@ -0,0 +1,13 @@
+package com.elias.mcsrb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsrbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McsrbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elias/mcsrb/McsrbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elias.mcsrb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsrbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
