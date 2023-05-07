89a6c25c59c34264de9c1a0486026384b9208b45
OrekiYuta <orekiyuta@gmail.com>
2023-05-08 03:16:51 +0800

    test(Stream): Add Stream usage test file

---
+++ src/test/java/com/elias/mcsrb/StreamUsageTest.java
@@ -0,0 +1,13 @@
+package com.elias.mcsrb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class StreamUsageTest {
+
+    @Test
+    void test() {
+        System.out.println("111");
+    }
+}
