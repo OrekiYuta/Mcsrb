5a9d331901f2cf7ea00b69a841de6a4782f6d03f
OrekiYuta <orekiyuta@gmail.com>
2023-10-05 01:24:45 +0800

    feat: add match usage

--- src/test/java/com/elias/mcsrb/junit/StreamUsageTest.java
+++ src/test/java/com/elias/mcsrb/junit/StreamUsageTest.java
@@ -3,6 +3,10 @@ package com.elias.mcsrb.junit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author OrekiYuta
  * @description Stream Usage
@@ -13,7 +17,38 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class StreamUsageTest {
 
     @Test
-    void test() {
-        System.out.println("111");
+    public void test() {
+
     }
+
+
+    /***
+     * @description anyMatch / allMatch / noneMatch
+     *
+     * @return void
+     * @author OrekiYuta
+     * @date 2023/10/5 00:58:09
+     */
+    @Test
+    public void testMatch() {
+
+//        test usage
+        List<String> list = new ArrayList<>();
+        boolean result1 = list.stream().anyMatch("A"::equals);
+        boolean result2 = list.stream().allMatch("B"::equals);
+        boolean result3 = list.stream().noneMatch("C"::equals);
+
+        System.out.println("result1: " + result1);
+        System.out.println("result2: " + result2);
+        System.out.println("result3: " + result3);
+        
+//        generic usage
+        List<String> stringList = Arrays.asList("aBC", "ddd");
+        boolean result4 = stringList.stream()
+                .filter(string -> string.startsWith("D"))
+                .allMatch(string -> string.contains("Z"));
+
+        System.out.println("result4: " + result4);
+    }
+
 }
